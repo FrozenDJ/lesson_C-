dc363fedf26b95d4498bc64a8edd1f87605256c8
Roman_Konkov <roman.konkoff@gmail.com>
2023-09-21 12:03:43 +0300

    add if/else prog

---
+++ Exp005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Write your name: ");
+string username = Console.ReadLine ();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Hey, it's dear MASHA");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
