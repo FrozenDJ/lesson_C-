236b5395365343f7ca6b46d5840e737c60698090
Roman_Konkov <roman.konkoff@gmail.com>
2023-09-26 18:17:19 +0400

    create Exp010_MethodArray

---
+++ Exp010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 1, 12, 31, 18, 15, 16, 17, 18 };
+int n = array.Length;
+int find = 18;
+int i = 0;
+
+while (i < n)
+{
+    if (array[i] == find)
+    {
+        Console.WriteLine(i);
+        break;
+    }
+    i++;
+}
