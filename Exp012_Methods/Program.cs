d5cbe2bf186dfd6af9030e9e7f243822f2497623
Roman_Konkov <roman.konkoff@gmail.com>
2023-09-26 20:00:54 +0400

    add to Exp012_Methods text task with method FOR

--- Exp012_Methods/Program.cs
+++ Exp012_Methods/Program.cs
@@ -32,16 +32,63 @@ int year = Method3();
 // Console.WriteLine(year);
 
 // Type 4
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+// string res = Method4(10, "abir");
+// Console.WriteLine(res);
+
 string Method4(int count, string text)
 {
-    int i = 0;
     string result = String.Empty;
-    while (i < count)
+    for (int i = 0; i < count; i++)
     {
         result = result + text;
-        i++;
     }
     return result;
 }
-string res = Method4(10, "abir");
+string res = Method4(10, "xxx");
 // Console.WriteLine(res);
+
+// for (int i = 2; i < 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} x {j} = {i * j}");
+//     }
+//     Console.WriteLine();
+// }
+
+// =====Работа стекстом=====
+// Дан текст. В тексте нужно заменить все пробелы чёррточками,
+// маленькие буквы "к" заменить большими "К",
+// а большие буквы "С" заменить маленькими "с".
+
+// Ясна ли задача?
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде, "
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = string.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
